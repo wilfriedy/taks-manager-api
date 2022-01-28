b55a10684b5daaaf892a6aa2c04eba30fc10fbd9
boydeveloper <wilfriedakpe3@gmail.com>
2022-01-28 04:28:24 +0100

    added delete and patch methods

--- controllers/tasksmethods.js
+++ controllers/tasksmethods.js
@@ -18,7 +18,39 @@ const createTask = async (req, res) => {
   }
 };
 
+const deleteTask = async (req, res) => {
+  try {
+    const { id: newId } = req.params;
+    const task = await Tasks.findOneAndDelete({ _id: newId });
+    if (!task) {
+      return res.status(404).json({ message: `Task with ${newId} not found` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
+
+// const update
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: newId } = req.params;
+    const task = await Tasks.findOneAndUpdate({ _id: newId }, req.body, {
+      new: true,
+    });
+    if (!task) {
+      return res.status(404).json({ message: `Task with ${newId} not found` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
+
 module.exports = {
   getTasks,
   createTask,
+  deleteTask,
+  updateTask,
 };
--- routes/tasksroutes.js
+++ routes/tasksroutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getTasks, createTask } = require("../controllers/tasksmethods");
+const {
+  getTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/tasksmethods");
 
 router.route("/").get(getTasks).post(createTask);
+router.route("/:id").delete(deleteTask).patch(updateTask);
 
 module.exports = router;
